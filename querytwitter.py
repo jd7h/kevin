cfeb3853d3aba9918461136ae976250c682c84e0
Judith van Stegeren <git@jd7h.com>
2016-12-29 23:03:06 +0100

    Extract relevant tweet data to a dictionary.
    Added save and load functions for csv, for quick access of data.
    I print the highest tweet id because that shows the newest tweet
    we processed. This is only for debug purposes.
    
    Todo: use Cursor object from Tweepy for processing /all/ relevant
    tweets for our query.

--- querytwitter.py
+++ querytwitter.py
@@ -1,3 +1,4 @@
+import csv
 import tweepy
 
 def open_config(filename):
@@ -14,6 +15,19 @@ def query_twitter(api):
     search_string = "media.ccc.de/v/33c3"
     results = api.search(search_string)
     return results
+
+def read_data(filename):
+    results = []
+    with open(filename,"r",newline='') as infile:
+        reader = csv.DictReader(infile,["tweet_id","tweet_time","nr_of_retweets","media_urls"])
+        for row in reader:
+            results.append(row)
+    return results
+
+def write_data(results,filename):
+    with open(filename,"w",newline='') as outfile:
+        writer = csv.DictWriter(outfile,["tweet_id","tweet_time","nr_of_retweets","media_urls"])
+        writer.writerows(results)
 
 def main():
     config = open_config("twitterauth.conf")
@@ -46,15 +60,30 @@ def main():
     '''
 
     # poc 3: extract metadata and media.ccc.de urls from tweets
-    results = query_twitter(api)
-    original_tweets = [tweet._json for tweet in results if "retweeted_status" not in tweet._json.keys()]
+    query_result = query_twitter(api)
+    #original_tweets = [tweet._json for tweet in query_result if "retweeted_status" not in tweet._json.keys()]
+    original_tweets = [tweet._json for tweet in query_result]
+    results = []
     for ot in original_tweets:
-        tweet_id, tweet_time, retweets = ot["id"], ot["created_at"], ot["retweet_count"]
-        media_urls = [url["expanded_url"] for url in ot["entities"]["urls"] if "media.ccc" in url["expanded_url"]]
-        print(tweet_id, tweet_time, retweets, media_urls)
-    print("Done.")
+        datapoint = {}
+        datapoint["tweet_id"], datapoint["tweet_time"], datapoint["nr_of_retweets"] = ot["id"], ot["created_at"], ot["retweet_count"]
+        datapoint["media_urls"] = [url["expanded_url"] for url in ot["entities"]["urls"] if "media.ccc" in url["expanded_url"]]
+        if len(datapoint["media_urls"]) > 0:
+            results.append(datapoint)
+            for key in ["tweet_id", "tweet_time", "nr_of_retweets", "media_urls"]:
+                print(datapoint[key],"")
+            print()
 
-    return results 
+    write_data(results,"results.data")
+
+    highest_id = 0
+    for r in results:
+        if r["tweet_id"] > highest_id:
+            highest_id = r["tweet_id"]
+
+    print("Highest tweet id:",highest_id)
+
+    return query_result,results
 
 
 if __name__ == "__main__":
