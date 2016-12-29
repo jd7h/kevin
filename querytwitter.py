4a2a555b0e09af6e2c9b04df3daea619acb04cb8
Judith van Stegeren <git@jd7h.com>
2016-12-29 19:39:50 +0100

    Add POC for extracting media.ccc.de urls.
    Todo: use Twitter API to get _all_ relevant tweets, not just the
    first 15. Maybe use streaming API?

--- querytwitter.py
+++ querytwitter.py
@@ -10,7 +10,12 @@ def open_config(filename):
             config[key] = value
     return config
 
-def __main__():
+def query_twitter(api):
+    search_string = "media.ccc.de/v/33c3"
+    results = api.search(search_string)
+    return results
+
+def main():
     config = open_config("twitterauth.conf")
     print(config)
     auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
@@ -18,9 +23,38 @@ def __main__():
 
     api = tweepy.API(auth)
 
+    ''' 
+    # poc 1
     public_tweets = api.home_timeline()
     for tweet in public_tweets:
         print(tweet.text)
+    '''
+
+    '''
+    # poc 2: print followers
+    def limit_handled(cursor):
+        while True:
+            try:
+                yield cursor.next()
+            except tweepy.RateLimitError:
+                print("Rate limit encountered!")
+                time.sleep(15 * 60)
+
+    for follower in limit_handled(tweepy.Cursor(api.followers).items()):
+        if follower.friends_count < 300:
+            print(follower.screen_name)
+    '''
+
+    # poc 3: extract metadata and media.ccc.de urls from tweets
+    results = query_twitter(api)
+    original_tweets = [tweet._json for tweet in results if "retweeted_status" not in tweet._json.keys()]
+    for ot in original_tweets:
+        tweet_id, tweet_time, retweets = ot["id"], ot["created_at"], ot["retweet_count"]
+        media_urls = [url["expanded_url"] for url in ot["entities"]["urls"] if "media.ccc" in url["expanded_url"]]
+        print(tweet_id, tweet_time, retweets, media_urls)
+    print("Done.")
+
+    return results 
 
 
 if __name__ == "__main__":
