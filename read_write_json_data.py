44804521f90617ffadbf2212e17ca7870343c048
Judith van Stegeren <git@jd7h.com>
2016-12-30 15:24:48 +0100

    Define 'append' function for json data files

--- read_write_json_data.py
+++ read_write_json_data.py
@@ -10,3 +10,12 @@ def write_data(results,filename):
     with open(filename,"w") as outfile:
         outfile.write(json.dumps(results))
         print(len(results),"written to",filename)
+
+def append_data(results,filename):
+    with open(filename,"r") as infile:
+        earlier_results = json.loads(infile.read())
+    with open(filename,"w") as outfile:
+        total_results = earlier_results + results
+        outfile.write(json.dumps(total_results))
+        print(len(earlier_results),"earlier tweets written to",filename)
+        print(len(results),"new tweets written to",filename)
