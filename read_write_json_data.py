92482732e760289bcfd63d207ddba3bac7a62559
Judith van Stegeren <git@jd7h.com>
2016-12-30 15:24:17 +0100

    Print feedback during reading and writing.

--- read_write_json_data.py
+++ read_write_json_data.py
@@ -3,8 +3,10 @@ import json
 def read_data(filename):
     with open(filename,"r") as infile:
         results = json.loads(infile.read())
+        print(len(results),"tweets read from",filename)
         return results
 
 def write_data(results,filename):
     with open(filename,"w") as outfile:
         outfile.write(json.dumps(results))
+        print(len(results),"written to",filename)
