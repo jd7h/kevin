cbd228a2dbef5b238618fcae3a0474b4e1458e6a
Judith van Stegeren <git@jd7h.com>
2016-12-30 17:08:23 +0100

    Add Twitter Streaming API support for obtaining new tweets.

--- query_twitter.py
+++ query_twitter.py
@@ -49,7 +49,7 @@ def query_twitter(api,search_string="media.ccc.de/v/32c3",newer_than_id=0):
     temp_results_filename = "last_query_results.data"
     results = []
     processed_tweets = 0
-    interval = 60
+    interval = 1000
     
     if newer_than_id != 0:
         cursor = tweepy.Cursor(api.search, q=search_string, since_id=newer_than_id).items()
@@ -72,64 +72,32 @@ def query_twitter(api,search_string="media.ccc.de/v/32c3",newer_than_id=0):
         rw.write_data(results,temp_results_filename)
     return results
 
+class StdOutListener(tweepy.StreamListener):
+    def on_status(self, data):
+        print(data)
+        return True
+
+    def on_error(self, data):
+        print(data)
+
 def main():
     config = open_config("twitterauth.conf")
-    print(config)
+    #print(config)
+
+    # source: http://docs.tweepy.org/en/v3.5.0/getting_started.html
     auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
     auth.set_access_token(config["access_token_key"], config["access_token_secret"])
 
     api = tweepy.API(auth)
+    
+    # source: http://docs.tweepy.org/en/v3.5.0/streaming_how_to.html
+    l = StdOutListener()
+    stream = tweepy.Stream(api.auth, l)
 
-    ''' 
-    # poc 1
-    public_tweets = api.home_timeline()
-    for tweet in public_tweets:
-        print(tweet.text)
-    '''
-
-    '''
-    # poc 2: print followers
-    def limit_handled(cursor):
-        while True:
-            try:
-                yield cursor.next()
-            except tweepy.RateLimitError:
-                print("Rate limit encountered!")
-                time.sleep(15 * 60)
-
-    for follower in limit_handled(tweepy.Cursor(api.followers).items()):
-        if follower.friends_count < 300:
-            print(follower.screen_name)
-    '''
-
-    '''
-    # poc 3: extract metadata and media.ccc.de urls from tweets
-    query_results = query_twitter(api)
-    #original_tweets = [tweet._json for tweet in query_result if "retweeted_status" not in tweet._json.keys()]
-    original_tweets = [tweet._json for tweet in query_result]
-    results = []
-    for ot in original_tweets:
-        datapoint = {}
-        datapoint["tweet_id"], datapoint["tweet_time"], datapoint["nr_of_retweets"] = ot["id"], ot["created_at"], ot["retweet_count"]
-        datapoint["media_urls"] = [url["expanded_url"] for url in ot["entities"]["urls"] if "media.ccc/v/33c3" in url["expanded_url"]]
-        if len(datapoint["media_urls"]) > 0:
-            results.append(datapoint)
-            for key in ["tweet_id", "tweet_time", "nr_of_retweets", "media_urls"]:
-                print(datapoint[key],"")
-            print()
-
-    rw.write_data(results,"results.data")
-
-    highest_id = 0
-    for r in results:
-        if r["tweet_id"] > highest_id:
-            highest_id = r["tweet_id"]
-
-    print("Highest tweet id:",highest_id)
-
-    return query_result,results
-    '''
-    return api
+    search_string = "33c3"
+    stream.filter(track=[search_string])
+    
+    #return api, myStream
 
 if __name__ == "__main__":
     main()
